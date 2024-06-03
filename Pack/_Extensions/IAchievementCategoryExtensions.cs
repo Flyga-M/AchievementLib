b4d9203cd38b2f1a485eec4707c7b4ce441dde44
Yannick Yannick <yannickjanssen@gmx.de>
2024-06-03 23:55:46 +0200

    added GetActions extensions methods to IAchievementCollection and IAchievementCategory

---
+++ Pack/_Extensions/IAchievementCategoryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AchievementLib.Pack
+{
+    /// <summary>
+    /// Extensions for the <see cref="IAchievementCategory"/> interface.
+    /// </summary>
+    public static class IAchievementCategoryExtensions
+    {
+        /// <summary>
+        /// Returns the <see cref="IAction"/>s associated with the <see cref="IAchievementCategory"/>.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns>The <see cref="IAction"/>s associated with the <see cref="IAchievementCategory"/>.</returns>
+        public static IAction[] GetActions(this IAchievementCategory category)
+        {
+            List<IAction> actions = new List<IAction>();
+
+            foreach (IAchievementCollection collection in category.AchievementCollections)
+            {
+                actions.AddRange(collection.GetActions());
+            }
+
+            return actions.ToArray();
+        }
+    }
+}
---
+++ Pack/_Extensions/IAchievementCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AchievementLib.Pack
+{
+    /// <summary>
+    /// Extensions for the <see cref="IAchievementCollection"/> interface.
+    /// </summary>
+    public static class IAchievementCollectionExtensions
+    {
+        /// <summary>
+        /// Returns the <see cref="IAction"/>s associated with the <see cref="IAchievementCollection"/>.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns>The <see cref="IAction"/>s associated with the <see cref="IAchievementCollection"/>.</returns>
+        public static IAction[] GetActions(this IAchievementCollection collection)
+        {
+            List<IAction> actions = new List<IAction>();
+
+            foreach (IAchievement achievement in collection.Achievements)
+            {
+                actions.AddRange(achievement.GetActions());
+            }
+
+            return actions.ToArray();
+        }
+    }
+}
